57e114e7b4291e5db13c2726e0038fb10f28da1e
Mihlalindovane <indovane18@gmail.com>
2024-11-11 08:16:37 +0200

    first commit

---
+++ My-Portfolio-main/JavaScript/Portfolio website.js
@@ -0,0 +1,12 @@
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+
+    alert('Thank you for contacting, Mihlali will get back to you soon!');
+
+document.querySelector('#fname').value = ''
+document.querySelector('#surname').value = '';
+document.querySelector('#email address').value = '';
+document.querySelector('#message').value = '';
+
+});
+
+
